3993fa2d74afac5225be3311ee21cfa8310c99d8
bneuhausz <bneuhausz@gmail.com>
2016-12-12 10:35:44 +0100

    database admin, master layout
    database admin, master layout

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>@yield('title')</title>
+        <link rel="stylesheet" href="{{ URL::to('css/bootstrap-3.3.7-dist/css/bootstrap.min.css') }}">
+        @yield('styles')
+    </head>
+    <body>
+        @include('partials.header')
+        <div class="container">
+            @yield('content')
+        </div>
+        <script src="URL::to('js/jquery-3.1.1.js')"></script>
+        <script src="URL::to('css/bootstrap-3.3.7-dist/js/bootstrap.min.js')"></script>
+        @yield('scripts')
+    </body>
+</html>
