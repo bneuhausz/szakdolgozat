0cd98eaa79ea932deb48d9dad17e45dc5c7161c4
bneuhausz <bneuhausz@gmail.com>
2016-12-14 10:10:08 +0100

    one more localized view
    one more localized view

--- resources/lang/en/general.php
+++ resources/lang/en/general.php
@@ -8,5 +8,7 @@
 		'submit' => 'Submit',
 		'forgottenPassword' => 'Forgot Your Password?',
 		'resetPassword' => 'Reset Password',
-		'sendResetLink' => 'Send Password Reset Link'
+		'sendResetLink' => 'Send Password Reset Link',
+		'resetEmailTextLine1' => 'You are receiving this email because we received a password reset request for your account.',
+		'resetEmailTextLine2' => 'If you did not request a password reset, no further action is required.'
 ];
