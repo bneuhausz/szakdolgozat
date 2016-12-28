f9df3a1217db333e5a37333b996e56bffa916220
bneuhausz <bneuhausz@gmail.com>
2016-12-28 22:07:34 +0100

    bans table migration added
    bans table migration added

---
+++ database/migrations/2016_12_28_210057_create_bans_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bans', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('admin_id');
+            $table->dateTime('end_date');
+            $table->text('reason');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
